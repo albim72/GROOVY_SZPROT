8df0aea433176b61b7d439b98a8a81de14c900f6
albim72 <marcin.albiniak72@gmail.com>
2026-01-07 14:19:39 +0100

    Update closure.groovy

--- DZIEN_1/GROOVY/closure.groovy
+++ DZIEN_1/GROOVY/closure.groovy
@@ -1,1 +1,15 @@
+def inc = {it+1}
+def res1 = [1,2,3,4].collect(inc)
 
+println(res1)
+println(inc(56))
+
+def absRef = Math.&abs
+def res2 = [-2,-6,-34,-15].collect(absRef)
+
+println(res2)
+
+def applyTwice = {x,f -> f(f(x))}
+def res3 = applyTwice(3,inc)
+
+println(res3)
