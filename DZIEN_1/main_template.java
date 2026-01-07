f735abb0dbcc6ea344bb4a1a740fd3fbd7e0b5da
albim72 <marcin.albiniak72@gmail.com>
2026-01-07 10:08:51 +0100

    Create main_template.java

---
+++ DZIEN_1/main_template.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")
+package ${PACKAGE_NAME};
+#end
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(">> Application started");
+    }
+}
